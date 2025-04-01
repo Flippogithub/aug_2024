b948b0328e97e68545c4d77659f8dc14dc793547
DANFLIPPO <dkflippo@ksu.edu>
2025-04-01 10:02:25 -0500

    added in hardware interface to xml file

--- build/BMC_2024/colcon_command_prefix_build.sh
+++ build/BMC_2024/colcon_command_prefix_build.sh
@@ -1,2 +1,1 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/dkflippo/aug2024/install/gazebo_ros2_control/share/gazebo_ros2_control/package.sh"
--- src/aug_2024/build/aug_2024/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dkflippo/aug2024/src/aug_2024
-# Build directory: /home/dkflippo/aug2024/src/aug_2024/build/aug_2024
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
