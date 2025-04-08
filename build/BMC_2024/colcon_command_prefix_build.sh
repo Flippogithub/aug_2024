3b65c1a5aa44bedc2e24fd90f4a4c9dc39ded440
DANFLIPPO <dkflippo@ksu.edu>
2025-04-08 09:57:36 -0500

    changed BMS package

--- build/BMC_2024/colcon_command_prefix_build.sh
+++ build/BMC_2024/colcon_command_prefix_build.sh
@@ -1,1 +1,2 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/dkflippo/aug2024/install/sllidar_ros2/share/sllidar_ros2/package.sh"
--- build/aug_2024/colcon_command_prefix_build.sh
+++ build/aug_2024/colcon_command_prefix_build.sh
@@ -1,3 +1,3 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/dkflippo/aug2024/install/BMC_2024/share/BMC_2024/package.sh"
 . "/home/dkflippo/aug2024/install/sllidar_ros2/share/sllidar_ros2/package.sh"
+. "/home/dkflippo/aug2024/install/BMC_2024/share/BMC_2024/package.sh"
